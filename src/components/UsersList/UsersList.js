0e3d81ab276d7202db25b4784478bde78908b4db
Katrin Dav <k.dawidowicz2016@gmail.com>
2021-03-09 23:47:06 +0100

    add styled-components, create basic styles

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { users } from '../../data/users';
 import User from '../User/User';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    background-color: #fff;
+    width: 100%;
+    max-width: 500px;
+    padding: 40px 30px;
+    border-radius: 25px;
+    box-shadow: 0 5px 15px -10px rgba(0,0,0, 0.3);
+    margin: 20px 0;
+
+`;
+
 
 const UsersList = () => {
     return (
         <>
-            <div>
+            <Wrapper>
                 <ul>
                     {users.map((userData) => (
                         <User userData={userData} />
                     ))}
 
                 </ul>
-            </div>
+            </Wrapper>
         </>
     );
 }
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import UsersList from '../components/UsersList/UsersList';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+  background-color: #f7f8fa;
+`;
 
 function Root() {
   return (
-    <>
+    <Wrapper>
       <UsersList />
 
-    </>
+    </Wrapper>
   );
 }
 
