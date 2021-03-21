5c6d89f7cfa5d7a13e64c9fde3574302bb5f6f91
Katrin Dav <k.dawidowicz2016@gmail.com>
2021-03-21 12:24:10 +0100

    change values in theme

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -7,8 +7,12 @@ export const theme = {
         success: '#8FCB81',
         error: '#CB8581',
         warning: '#E1D888',
+        darkGrey: '#737C8E',
+        lightPurple: '#ECEFF7',
+        darkPurple: '#C0C7D6',
     },
     fontSize: {
+        xl: '24px',
         l: '17px',
         m: '13px',
         s: '11px',
